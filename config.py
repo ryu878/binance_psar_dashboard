ce9acd6be87028c0403d22a8399a90d7ab61a324
Ryan Hayabusa <81808867+ryu878@users.noreply.github.com>
2022-10-29 22:33:31 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# Binance API keys
+key = 'CaZjoABwbGcniQkQbAhjFBUAxs552vhbujC5o5HNBFEKbzdsKe1HEHm3sXUkEnDs'
+secret = 'XQjeZzytmaAfDNELzZsy9EUno20i4eAtsajhNV2TI6tdUSuFFyOntOmYTjjDIkuT'
+
+# Assets
+asset1 = 'BTCUSDT'
+
+# PSAR settings
+iaf = 0.02
+maxaf = 0.2
+
+# Timeframe settings
+htf = '30m' # 1d
+ltf = '5m' # 4h
+
+limit = 1440
+
+q1 = 15
+q2 = 15
